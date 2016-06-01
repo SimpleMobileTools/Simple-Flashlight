92d61fb2d157ea9d783b955a8cf4131de7631617
tibbi <tibor@kaputa.sk>
2016-06-01 17:34:51 +0200

    add About section

--- app/src/main/java/com/simplemobiletools/flashlight/MainActivity.java
+++ app/src/main/java/com/simplemobiletools/flashlight/MainActivity.java
@@ -1,7 +1,11 @@
 package com.simplemobiletools.flashlight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -18,6 +22,25 @@ public class MainActivity extends AppCompatActivity implements MyCamera {
 
         setupToggleButton();
         setupCameraImpl();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                final Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setupCameraImpl() {
