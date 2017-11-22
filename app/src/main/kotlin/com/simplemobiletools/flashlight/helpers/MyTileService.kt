c4bc06e3bcc3b8c7d563fb21f006fcdf9656ed74
Christopher Jakob <jakob.chrisli@gmail.com>
2017-11-22 21:20:05 +0100

    Use correct icon for quicksettings tile
    - It will be tinted by the system automatically
    
    refs #31

--- app/src/main/kotlin/com/simplemobiletools/flashlight/helpers/MyTileService.kt
+++ app/src/main/kotlin/com/simplemobiletools/flashlight/helpers/MyTileService.kt
@@ -1,6 +1,7 @@
 package com.simplemobiletools.flashlight.helpers
 
 import android.os.Build
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.support.annotation.RequiresApi
 
@@ -8,5 +9,8 @@ import android.support.annotation.RequiresApi
 class MyTileService : TileService() {
     override fun onClick() {
         MyCameraImpl.newInstance(this).toggleFlashlight()
+
+        qsTile.state = if (MyCameraImpl.isFlashlightOn) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
     }
 }
