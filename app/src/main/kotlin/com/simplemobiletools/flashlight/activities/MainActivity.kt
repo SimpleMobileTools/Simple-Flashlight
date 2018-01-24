2e938cce1e501c1cc3ac23f6b1e04b956aac45b6
tibbi <tibor@kaputa.sk>
2018-01-24 22:23:30 +0100

    update commons to 3.8.8

--- app/src/main/kotlin/com/simplemobiletools/flashlight/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/flashlight/activities/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.SeekBar
