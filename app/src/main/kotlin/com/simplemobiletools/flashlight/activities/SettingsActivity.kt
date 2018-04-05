789b66b9ed57659f6948e56348f5b303cf14ec60
tibbi <tibor@kaputa.sk>
2018-04-05 13:37:34 +0200

    add a toggle button for avoiding showing Whats New

--- app/src/main/kotlin/com/simplemobiletools/flashlight/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/flashlight/activities/SettingsActivity.kt
@@ -19,6 +19,7 @@ class SettingsActivity : SimpleActivity() {
 
         setupCustomizeColors()
         setupUseEnglish()
+        setupAvoidWhatsNew()
         setupTurnFlashlightOn()
         setupBrightDisplay()
         setupStroboscope()
@@ -38,6 +39,14 @@ class SettingsActivity : SimpleActivity() {
             settings_use_english.toggle()
             config.useEnglish = settings_use_english.isChecked
             System.exit(0)
+        }
+    }
+
+    private fun setupAvoidWhatsNew() {
+        settings_avoid_whats_new.isChecked = config.avoidWhatsNew
+        settings_avoid_whats_new_holder.setOnClickListener {
+            settings_avoid_whats_new.toggle()
+            config.avoidWhatsNew = settings_avoid_whats_new.isChecked
         }
     }
 
