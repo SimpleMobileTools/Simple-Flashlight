dde4c7ac600ecc9c35e645c6dc59465e4fad65d8
tibbi <tibor@kaputa.sk>
2018-08-10 23:56:20 +0200

    update commons to 4.6.5

--- app/src/main/kotlin/com/simplemobiletools/flashlight/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/flashlight/activities/MainActivity.kt
@@ -50,6 +50,7 @@ class MainActivity : SimpleActivity() {
         }
 
         setupStroboscope()
+        checkAppOnSDCard()
     }
 
     override fun onResume() {
@@ -132,13 +133,15 @@ class MainActivity : SimpleActivity() {
     }
 
     private fun launchAbout() {
+        val licenses = LICENSE_OTTO
+
         val faqItems = arrayListOf(
                 FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
                 FAQItem(R.string.faq_4_title_commons, R.string.faq_4_text_commons),
                 FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons)
         )
 
-        startAboutActivity(R.string.app_name, LICENSE_OTTO, BuildConfig.VERSION_NAME, faqItems)
+        startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
 
     private fun setupCameraImpl() {
