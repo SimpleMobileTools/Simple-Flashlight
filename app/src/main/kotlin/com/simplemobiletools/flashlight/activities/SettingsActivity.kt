07887cf149ae796a20ff275cb3a48476dfd852e5
tibbi <tibor@kaputa.sk>
2021-11-13 12:51:01 +0100

    updating commons and some style

--- app/src/main/kotlin/com/simplemobiletools/flashlight/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/flashlight/activities/SettingsActivity.kt
@@ -30,6 +30,14 @@ class SettingsActivity : SimpleActivity() {
         setupForcePortrait()
         updateTextColors(settings_holder)
         invalidateOptionsMenu()
+
+        arrayOf(settings_color_customization_label, settings_general_settings_label).forEach {
+            it.setTextColor(getAdjustedPrimaryColor())
+        }
+
+        arrayOf(settings_color_customization_holder, settings_general_settings_holder).forEach {
+            it.background.applyColorFilter(baseConfig.backgroundColor.getContrastColor())
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,6 +47,12 @@ class SettingsActivity : SimpleActivity() {
 
     private fun setupPurchaseThankYou() {
         settings_purchase_thank_you_holder.beGoneIf(isOrWasThankYouInstalled())
+
+        // make sure the corners at ripple fit the stroke rounded corners
+        if (settings_purchase_thank_you_holder.isGone()) {
+            settings_use_english_holder.background = resources.getDrawable(R.drawable.ripple_top_corners, theme)
+        }
+
         settings_purchase_thank_you_holder.setOnClickListener {
             launchPurchaseThankYouIntent()
         }
@@ -63,6 +77,11 @@ class SettingsActivity : SimpleActivity() {
     private fun setupUseEnglish() {
         settings_use_english_holder.beVisibleIf(config.wasUseEnglishToggled || Locale.getDefault().language != "en")
         settings_use_english.isChecked = config.useEnglish
+
+        if (settings_use_english_holder.isGone() && settings_purchase_thank_you_holder.isGone()) {
+            settings_turn_flashlight_on_holder.background = resources.getDrawable(R.drawable.ripple_top_corners, theme)
+        }
+
         settings_use_english_holder.setOnClickListener {
             settings_use_english.toggle()
             config.useEnglish = settings_use_english.isChecked
