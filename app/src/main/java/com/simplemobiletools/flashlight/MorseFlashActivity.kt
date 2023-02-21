f1abc4dfbbc40557e1195467dee36fd44a019f21
bsaesilva <bsaebno19@gmail.com>
2023-02-21 13:16:51 +0100

    remove_comment

--- app/src/main/java/com/simplemobiletools/flashlight/MorseFlashActivity.kt
+++ app/src/main/java/com/simplemobiletools/flashlight/MorseFlashActivity.kt
@@ -11,35 +11,10 @@ class MorseFlashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_morse_flash)
 
-        /*
-                  u = 200L
-         DEFAULT LENGTH DASH = u * 3     ---> delai 3 pour un -
-         DEFAULT DELAY LETTERS = u * 3   ---> delai 3 entre les lettres
-         DEFAULT DELAY WORDS = u * 7     ---> delai entre les mots
-
-         u = .(point) après u = pause     u*3= pause entre les lettre après u*3 = - (tiret)
-
-                      .     .     .           -         -         -           .     .     .
-    SOS = arrayListOf(u, u, u, u, u, u * 3, u * 3, u, u * 3, u, u * 3, u * 3, u, u, u, u, u, u * 7)
-                         p     p      !            p         p          !        p     p     pause entre les mots
-                                      V                                 V
-                                   pause entre les lettres           pause entre les lettres
-
-
-      pl = pause entre les lettres
-      pm = pause entre les mots
-                     .    p    .    p    .    pl   -    p    -    p    -   pl    .    p    .    p    .    pm
-  MSG FLASH  ====> [200, 200, 200, 200, 200, 600, 600, 200, 600, 200, 600, 600, 200, 200, 200, 200, 200, 1400]
-         */
-
-         var _u = 200L //  DEFAULT LENGTH ONE UNIT MILLISECONDS
-        val MSG = arrayListOf(_u) // Futur message flashing morse code
-
+         var _u = 200L
+        val MSG = arrayListOf(_u)
         val editText_UserInput = findViewById<EditText>(R.id.editText_UserInput)
-
-        // for morse translator . and -
         val morseAlphabet = mapOf(
-            // letters lowercase
             'a' to ".-",
             'b' to "-...",
             'c' to "-.-.",
@@ -66,7 +41,6 @@ class MorseFlashActivity : AppCompatActivity() {
             'x' to "-..-",
             'y' to "-.--",
             'z' to "--..",
-            // letters uppercase
             'A' to ".-",
             'B' to "-...",
             'C' to "-.-.",
@@ -93,7 +67,6 @@ class MorseFlashActivity : AppCompatActivity() {
             'X' to "-..-",
             'Y' to "-.--",
             'Z' to "--..",
-            // numbers
             '1' to ".----",
             '2' to "..---",
             '3' to "...--",
@@ -106,19 +79,19 @@ class MorseFlashActivity : AppCompatActivity() {
             '0' to "-----"
         )
 
-        var resultMorseCode = " " // futur result of translation
+        var resultMorseCode = " "
 
         val cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
         val cameraId = cameraManager.cameraIdList[0]
 
 
         btn_morseFlash.setOnClickListener{
-            val text_user = editText_UserInput.text //get text of input
+            val text_user = editText_UserInput.text
 
             System.out.println("Saisie Utilisateur ====> "+ text_user)
 
             resultMorseCode = text_user.map { if (it == ' ') "/" else morseAlphabet[it] }.joinToString(" ")
-            textView_translation.setText(resultMorseCode) // set the result of translation on TextView
+            textView_translation.setText(resultMorseCode)
 
             System.out.println("Traduction MORSE ====> "+ resultMorseCode)
 
@@ -130,32 +103,32 @@ class MorseFlashActivity : AppCompatActivity() {
                 if (resultMorseCode[i] == '/'){
                     cameraManager.setTorchMode(cameraId, false)
                     System.out.println("flash OFF")
-                    MSG.add(_u*7) // DEFAULT DELAY WORDS = u * 7 // flash OFF
+                    MSG.add(_u*7)
                     Thread.sleep(_u*7)
                 }
                 else if (resultMorseCode[i]==' '){
                     cameraManager.setTorchMode(cameraId, false)
                     System.out.println("flash OFF")
-                    if(resultMorseCode[i+1]!='/' && resultMorseCode[i-1]!='/'){// flash OFF
+                    if(resultMorseCode[i+1]!='/' && resultMorseCode[i-1]!='/'){
                         Thread.sleep(_u*3)
-                        MSG.add(_u*3) // DEFAULT DELAY LETTERS = u * 3
+                        MSG.add(_u*3)
                     }}
 
                 else if (resultMorseCode[i]=='-'){
-                    cameraManager.setTorchMode(cameraId, true) // flash ON
+                    cameraManager.setTorchMode(cameraId, true)
                     System.out.println("flash ON")
-                    MSG.add(_u*3) // DEFAULT LENGTH DASH = u * 3
+                    MSG.add(_u*3)
                     Thread.sleep(_u*3)
                     cameraManager.setTorchMode(cameraId, false)
                     System.out.println("flash Off")
                     if(i+1<resultMorseCode.length){
-                        if(resultMorseCode[i+1]!=' '){ // flash OFF
+                        if(resultMorseCode[i+1]!=' '){
                             Thread.sleep(_u)
                             MSG.add(_u)
                         }}}
 
                 else if (resultMorseCode[i]=='.'){
-                    cameraManager.setTorchMode(cameraId, true) // flash ON
+                    cameraManager.setTorchMode(cameraId, true)
                     System.out.println("flash ON")
                     MSG.add(_u)
                     Thread.sleep(_u)
@@ -164,7 +137,7 @@ class MorseFlashActivity : AppCompatActivity() {
                     if(i+1<resultMorseCode.length){
                         if(resultMorseCode[i+1]!=' '){
                             Thread.sleep(_u)
-                            MSG.add(_u) // DEFAULT DELAY light off in a letter = u * 3
+                            MSG.add(_u)
                         }}}
             }
             MSG.removeLast()
@@ -173,78 +146,3 @@ class MorseFlashActivity : AppCompatActivity() {
         }
     }
 }
-
-//Je pense que c'est correct , mais reste à etre améliorer le flash marche sur mon téléphone
-//on supprimera les commentaires par la suite
-//I think it's correct , but still have to be improved the flash works correctly on my phone
-//comments will be deleted later.
-
-/*    OUTPUT CONSOLE ==> FOR SOS MESSAGE
-I/System.out: Saisie Utilisateur ====> sos
-I/System.out: Traduction MORSE ====> ... --- ...
-I/System.out: MSG vide ====> []
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: MESSAGE  FLASH  ====> [200, 200, 200, 200, 200, 600, 600, 200, 600, 200, 600, 600, 200, 200, 200, 200, 200, 1400]
-
-
-Je pense que c'est correct , mais reste à etre améliorer le flash marche sur mon téléphone
-I think it's correct , but still have to be improved the flash works correctly on my phone
-
- */
-
-/*    OUTPUT CONSOLE FOR MESSAGE ==> test sos (same TEST SOS)
-
-I/System.out: Saisie Utilisateur ====> test sos
-I/System.out: Traduction MORSE ====> - . ... - / ... --- ...
-I/System.out: MSG vide ====> []
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: flash ON
-I/System.out: flash OFF
-I/System.out: MESSAGE  FLASH  ====> [600, 600, 200, 600, 200, 200, 200, 200, 200, 600, 600, 600, 600, 200, 200, 200, 200, 200, 600, 600, 200, 600, 200, 600, 600, 200, 200, 200, 200, 200, 1400]
-
- */
